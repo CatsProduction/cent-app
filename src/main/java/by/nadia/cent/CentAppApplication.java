cff143c0269487da49208c306346dbdc83e4d58a
Nadia Shokel <primisen17@gmail.com>
2022-05-10 18:25:47 +0300

    Initial project

---
+++ src/main/java/by/nadia/cent/CentAppApplication.java
@@ -0,0 +1,13 @@
+package by.nadia.cent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/nadia/cent/CentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nadia.cent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
