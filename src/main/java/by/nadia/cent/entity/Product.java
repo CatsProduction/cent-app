4a659977b774fd80cf56e1f7f23a5e232409e0fc
nadia <primisen17@gmail.com>
2022-05-20 17:20:10 +0300

    Initialized DB, added entities

---
+++ src/main/java/by/nadia/cent/entity/Product.java
@@ -0,0 +1,52 @@
+package by.nadia.cent.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "product")
+@RequiredArgsConstructor
+@Getter
+@Setter(AccessLevel.PUBLIC)
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_type_id", referencedColumnName = "id")
+    private ProductType type;
+
+    @ManyToOne
+    @JoinColumn(name = "manufacturer_id", referencedColumnName = "id")
+    private Manufacturer manufacturer;
+
+    @Column(name = "price", precision = 3, scale = 2, nullable = false)
+    private BigDecimal price;
+
+    @ManyToMany
+    @JoinColumn(name = "shop_id", referencedColumnName = "id")
+    private List<Shop> shop;
+
+    @Column(name = "price_update_date", nullable = false)
+    private LocalDate priceUpdateDate;
+}
