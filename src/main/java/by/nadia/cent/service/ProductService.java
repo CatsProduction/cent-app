60a92c4b98e0295063aa71aa761d9e2fdcbd20d3
nadia <primisen17@gmail.com>
2022-06-02 17:40:29 +0300

    Added functionality: save products

---
+++ src/main/java/by/nadia/cent/service/ProductService.java
@@ -0,0 +1,10 @@
+package by.nadia.cent.service;
+
+import by.nadia.cent.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    void saveAll(List<Product> products);
+}
